13e39b2c44187ea9b0a5b17537f0717f10f77c02
ryuuseijin <ryuuseijin@example.com>
2019-05-18 14:12:44 +1000

    chore: update readme

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
-(defproject chan-utils "0.1.0-SNAPSHOT"
+(defproject ryuuseijin/chan-utils "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/ryuuseijin/chan-utils"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :repositories [["releases" {:url "https://repo.clojars.org"}]]
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/core.async "0.4.490"]]
-  :repl-options {:init-ns chan-utils.core})
+  :repl-options {:init-ns ryuuseijin.chan-utils.core})
