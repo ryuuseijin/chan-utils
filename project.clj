dfc44b55ffe860061a398761441a647666ec9d13
ryuuseijin <ryuuseijin@example.com>
2019-06-01 19:44:33 +1000

    Version 0.1.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ryuuseijin/chan-utils "0.1.1"
+(defproject ryuuseijin/chan-utils "0.1.2-SNAPSHOT"
   :description "FIXME: write description"
   :url "https://github.com/ryuuseijin/chan-utils"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
